cb01b5792b8e3c0f3c4050b9fdbac956f7790459
Mahbub Zulkarnain <prof.bub@live.com>
2023-04-08 15:49:52 +0700

    test: add test telkomsel number

--- provider_test.go
+++ provider_test.go
@@ -1,95 +1,158 @@
-package phone_number
+package phonenumber
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
+func TestPhoneNumber_Provider_Telkomsel(t *testing.T) {
+	pns := []PhoneNumber{
+		"085100000000",
+		"085200000000",
+		"0853000000000",
+		"082310000000",
+
+		"08110000000",
+
+		"081200000000",
+		"081300000000",
+		"082100000000",
+		"082200000000",
+	}
+	for _, pn := range pns {
+		if !strings.Contains(pn.Provider(), "TELKOMSEL") {
+			t.Fatal("invalid value")
+		}
+		fmt.Println(pn.Provider())
+	}
+}
+
 func TestPhoneNumber_Provider_Telkomsel_AS(t *testing.T) {
-	pn := PhoneNumber("082318000000")
-	result := pn.Provider()
-	fmt.Println(result)
-	if result != "TELKOMSEL AS" {
-		t.Fatal("invalid value")
+	pns := []PhoneNumber{
+		"085100000000",
+		"085200000000",
+		"0853000000000",
+
+		"082310000000",
+	}
+	for _, pn := range pns {
+		if !strings.Contains(pn.Provider(), "AS") {
+			t.Fatal("invalid value")
+		}
+		fmt.Println(pn.Provider())
 	}
 }
 
 func TestPhoneNumber_Provider_Telkomsel_Simpati(t *testing.T) {
-	pn := PhoneNumber("082299552272")
-	result := pn.Provider()
-	fmt.Println(result)
-	if result != "TELKOMSEL SIMPATI" {
-		t.Fatal("invalid value")
+	pns := []PhoneNumber{
+		"081200000000",
+		"081300000000",
+		"082100000000",
+		"082200000000",
+	}
+	for _, pn := range pns {
+		if !strings.Contains(pn.Provider(), "SIMPATI") {
+			t.Fatal("invalid value")
+		}
+		fmt.Println(pn.Provider())
 	}
 }
+
 func TestPhoneNumber_Provider_Telkomsel_Halo(t *testing.T) {
-	pn := PhoneNumber("08115590999")
-	result := pn.Provider()
-	fmt.Println(result)
-	if result != "TELKOMSEL HALO" {
-		t.Fatal("invalid value")
+	pns := []PhoneNumber{
+		"08110000000",
+	}
+	for _, pn := range pns {
+		if !strings.Contains(pn.Provider(), "HALO") {
+			t.Fatal("invalid value")
+		}
+		fmt.Println(pn.Provider())
 	}
 }
 
 func TestPhoneNumber_Provider_Indosat_OOREDOO(t *testing.T) {
-	pn := PhoneNumber("085890005171")
-	result := pn.Provider()
-	fmt.Println(result)
-	if result != "INDOSAT OOREDOO" {
-		t.Fatal("invalid value")
+	pns := []PhoneNumber{
+		"085700000000",
+		"085800000000",
+	}
+	for _, pn := range pns {
+		if pn.Provider() != "INDOSAT OOREDOO" {
+			t.Fatal("invalid value")
+		}
+		fmt.Println(pn.Provider())
 	}
 }
 
 func TestPhoneNumber_Provider_XL(t *testing.T) {
-	pn := PhoneNumber("087868884504")
-	result := pn.Provider()
-	fmt.Println(result)
-	if result != "XL" {
-		t.Fatal("invalid value")
+	pns := []PhoneNumber{
+		"087868884504",
+	}
+	for _, pn := range pns {
+		if pn.Provider() != "XL" {
+			t.Fatal("invalid value", pn.Provider())
+		}
+		fmt.Println(pn.Provider())
 	}
 }
 
 func TestPhoneNumber_Provider_Axis(t *testing.T) {
-	pn := PhoneNumber("083862081192")
-	result := pn.Provider()
-	fmt.Println(result)
-	if result != "AXIS" {
-		t.Fatal("invalid value")
+	pns := []PhoneNumber{
+		"083862081192",
+	}
+	for _, pn := range pns {
+		if pn.Provider() != "AXIS" {
+			t.Fatal("invalid value", pn.Provider())
+		}
+		fmt.Println(pn.Provider())
 	}
 }
 
 func TestPhoneNumber_Provider_Three(t *testing.T) {
-	pn := PhoneNumber("089520032657")
-	result := pn.Provider()
-	fmt.Println(result)
-	if result != "THREE" {
-		t.Fatal("invalid value")
+	pns := []PhoneNumber{
+		"089520032657",
+	}
+	for _, pn := range pns {
+		if pn.Provider() != "THREE" {
+			t.Fatal("invalid value", pn.Provider())
+		}
+		fmt.Println(pn.Provider())
 	}
 }
 
 func TestPhoneNumber_Provider_SMARTFREN(t *testing.T) {
-	pn := PhoneNumber("088211122152")
-	result := pn.Provider()
-	fmt.Println(result)
-	if result != "SMARTFREN" {
-		t.Fatal("invalid value")
+	pns := []PhoneNumber{
+		"088211122152",
+	}
+	for _, pn := range pns {
+		if pn.Provider() != "SMARTFREN" {
+			t.Fatal("invalid value", pn.Provider())
+		}
+		fmt.Println(pn.Provider())
 	}
 }
 
 func TestPhoneNumber_Provider_TELKOM(t *testing.T) {
-	pn := PhoneNumber("0215386950")
-	result := pn.Provider()
-	fmt.Println(result)
-	if result != "TELKOM" {
-		t.Fatal("invalid value")
+	pns := []PhoneNumber{
+		"0215386950",
+	}
+	for _, pn := range pns {
+		if pn.Provider() != "TELKOM" {
+			t.Fatal("invalid value", pn.Provider())
+		}
+		fmt.Println(pn.Provider())
 	}
 }
 
 func TestPhoneNumber_Provider_Notfound(t *testing.T) {
-	pn := PhoneNumber("079520032657")
-	result := pn.Provider()
-	fmt.Println(result)
-	if result != "" || Provider(pn) != result {
-		t.Fatal("invalid value")
+	pns := []PhoneNumber{
+		"079520032657",
+	}
+	for _, pn := range pns {
+		result := pn.Provider()
+		if result != "" || Provider(pn) != result {
+			t.Fatal("invalid value")
+		}
+		fmt.Println(pn.Provider())
 	}
 }
